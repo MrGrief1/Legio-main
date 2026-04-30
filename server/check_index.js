5165a55f42a48cfd88d17dbeef177d30afa63789
MrGrief1 <maksurazov1502@gmail.com>
2026-04-30 21:29:21 +0300

    wad
    awd

--- server/check_index.js
+++
@@ -1,8 +0,0 @@
-const db = require('./database');
-
-db.all("SELECT name FROM sqlite_master WHERE type='index' AND name='idx_users_name'", [], (err, rows) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Indexes found:', rows);
-});
--- server/delete_user.js
+++
@@ -1,8 +0,0 @@
-const db = require('./database');
-
-db.run("DELETE FROM users WHERE id = 4", function(err) {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log(`Row(s) deleted ${this.changes}`);
-});
